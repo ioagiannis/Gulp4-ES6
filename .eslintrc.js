8313b79a4fba1533f4b09462ed0a4be8737da036
Ioannis Giannis <i.giannis@linakis.com>
2022-10-17 13:49:31 +0300

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,33 @@
 module.exports = {
   extends: [
     // add more generic rulesets here, such as:
-    // 'eslint:recommended',
+    'eslint:recommended',
     'plugin:vue/vue3-recommended',
     // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
     'prettier',
   ],
+  ignorePatterns: [
+    'public',
+    'node_modules',
+    '.eslintrc.js',
+    'webpack.*',
+    'src/js/vendors/*',
+    'src/mockServer/*',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    jquery: true,
+  },
   rules: {
     // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    'prefer-template': 1,
+    'prefer-const': 1,
+    'no-var': 1,
+    'no-unused-vars': 1,
+    camelcase: 1,
     'vue/no-v-html': 'off',
-    'prefer-const': 'warn',
+    'vue/multi-word-component-names': 'off',
     'sort-imports': [
       'error',
       {
